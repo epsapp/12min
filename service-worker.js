f4853aca6dbbb14526b74276f3fb6906c764fb6e
epsapp <julien.cornille@ac-lille.fr>
2026-02-14 09:17:20 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'course-athletisme-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+// Installation : mise en cache des fichiers
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Cache ouvert');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activation : nettoyage des anciens caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch : stratégie Cache First, puis réseau
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((response) => {
+        // Ne pas mettre en cache les requêtes non-GET ou les requêtes échouées
+        if (!response || response.status !== 200 || event.request.method !== 'GET') {
+          return response;
+        }
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      });
+    }).catch(() => {
+      // Fallback offline
+      return caches.match('./index.html');
+    })
+  );
+});
